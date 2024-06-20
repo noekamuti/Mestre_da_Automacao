c8cb47345f853c9e64ebbb28d6e4a0cad0d34583
Noe Kamuti <noesebastiao.kc@gmail.com>
2024-06-20 09:01:55 +0100

    20-06-2024 - COMBINAÇÃO DE TECLAS

---
+++ combinacao_teclas.py
@@ -0,0 +1,61 @@
+import pyautogui
+from time import sleep
+
+# Como usar combinações de teclas
+pyautogui.click(-1606,1238,duration=6)
+#Simular "Segurar uma tecla"
+
+pyautogui.keyDown('ctrl','a')
+pyautogui.press('c')
+pyautogui.keyUp('ctrl')
+sleep(2)
+'''
+
+#pyautogui.hotkey('ctrl','a')
+pyautogui.click(-3132,695,duration=6)
+pyautogui.hotkey('ctrl','v')
+
+
+#Simular "Segurar uma tecla"
+#pyautogui.click(-1485,1087,duration=6)
+
+pyautogui.keyDown('ctrl')
+pyautogui.keyDown('a')
+pyautogui.keyUp('ctrl')
+pyautogui.keyUp('a')
+'''
+
+
+# 19-06-2024 | 
+# Das 07:20 as 8:00
+# Tempo: 9:00 minutos de aula
+
+# Seguimos na automatização, mas sem falar muito para não perder o ritmo;
+
+# DESAFIO 1- Usar combinações de teclas e incluir no local especifico;
+# E lembrei de te lembrar que o Jonathan disse, que uma das formas das formas de dar certo, é mesmo ir fazendo todos os projectos
+# E posteriormente tambem, ir aplicando mesmo que aprentemente não tenhamos toda a expertise.
+# O que me leva a pensar que apartir de agosto, começarei a aplicar candidaturas de freelancer (Juntando com o trabalho dos 
+# diaconos em que estou a aprender sobre o predio espiritual/fisico)
+# Posso tambem partilhar o meu portfolio na maná global 
+# (Especialista em RPA com Python, SQL, Oracle, Power BI, Analista de Processos de Negócios)
+# Com portfolio em: 
+#   RPA com Python,
+#   Aplicações com Python,
+#   Gestão de Dados com SQL
+#   Criação de Vistas com Power BI
+#   Analista de Processos de Negócios BPM (n)
+
+# Como usar os botões e atalhos?
+# Passo 1: Importar a biblioteca 
+# Passo 2: Abrir o software de coordenadas MouseInfo 0.1.3 (Obter as coordenadas)
+    #-> Obter as coordenadas para cada uma das ações
+# Passo 3: Navegar e clicar no campo E-mail -> Mover o maouse ate a coordenada
+    #-> Incluir os comandos para que omouse mova-se ate la
+# Passo 4: Apertar TAB -> 
+# Passo 5: Arrastar ate ao ponto da senha e colar a mesma
+# Passo 6: Apertar TAB
+# Passo 7: Apertar Space
+# Passo 8: Importar a biblioteca para a utomatização: PYAUTO GUI e para Gestão do Tempo: SLEEP
+
+
